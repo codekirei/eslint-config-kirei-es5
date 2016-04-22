08adc97be7f843e822c3817f94907514877ab879
Jacob Blakely <jacob@codekirei.com>
2016-04-21 18:20:47 -0700

    add rules

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+module.exports = {
+  extends: [
+    './rules/best-practices.js',
+    './rules/node.js',
+    './rules/possible-errors.js',
+    './rules/strict-mode.js',
+    './rules/stylistic-issues.js',
+    './rules/variables.js',
+  ],
+}
